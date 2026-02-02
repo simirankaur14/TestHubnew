d018eb606a841ba80420317b9b39aa182c78dbd0
simiranjeet kaur <simiranjeet.kaur@dryicelabs.com>
2026-02-02 19:52:37 +0530

    files

---
+++ src/test/Login_5_Schedule_A1F10039954B9350DE97B93133663965.java
@@ -0,0 +1,105 @@
+/**@generated
+ * WARNING � Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+package test;
+
+import com.ibm.rational.test.lt.execution.http.tes.*;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.IKThinkControl;
+import com.ibm.rational.test.lt.kernel.action.impl.Container;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.action.impl.UserGroup;
+import com.ibm.rational.test.lt.kernel.data.FeatureOptionsDataAreaCreation;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class Login_5_Schedule_A1F10039954B9350DE97B93133663965
+		extends com.ibm.rational.test.lt.kernel.action.impl.Schedule {
+
+	public Login_5_Schedule_A1F10039954B9350DE97B93133663965(IContainer parent, String name) {
+		super(parent, name, "A1F10039954B9350DE97B93133663965");
+		super.actualScheduleName = "Login_5";
+		setThinkMax(10000);
+		setThinkScheme(IKThinkControl.RECORDED);
+
+	}
+
+	public void execute() {
+
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPPageTitleVPEvent(), null, 1, "Page title verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPResponseCodeVPEvent(), null, 1, "Response code verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPResponseSizeVPEvent(), null, 1, "Response size verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTFailVPEvent(), null, 1, "Content Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), null, 1, "Connection failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), null, 1, "Authentication failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(),
+				new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTTestVerdictFailedEvent(), null, 2, "Test verdict was set to fail"));
+		this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), null, 1, "Failed to extract reference"));
+		this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), null, 1, "Substitution failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), null, 1, "Timeout"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTDroppedConnectionEvent(), null, 1, "Server dropped connection"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), null, 1, "Custom Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(),
+				new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1,
+				"Custom Code reported an unhandled exception"));
+
+		this.addUserGroup(new UserGroup_1(this));
+
+		setMaxHealthFailuresToLog(50, -1, 0);
+		super.execute();
+	}
+
+	public class UserGroup_1 extends UserGroup {
+
+		public UserGroup_1(IContainer parent) {
+			super(parent, "Login5", "A1F10039954CA73CDE97B93133663965");
+
+			setNumUsers(5.0);
+
+		}
+
+		public IContainer createTesterWorkload(IContainer parent) {
+			return scenario_1(parent);
+		}
+
+		private Container scenario_1(IContainer parent) {
+			Container scenario = new Container(parent, "Default Scenario", "A1F10039954CA747DE97B93133663965") {
+
+				public void reportStopMessage() {
+				}
+
+				public void reportForcedStopMessage() {
+				}
+
+				public void execute() {
+					this.add(new test.Login_Test_A1F10029B6C32EE0D8E4B73564616536(this,
+							"A1F100399F4F5B70DE97B93133663965") {
+						public void execute() {
+							this.setRtbEnabled(false);
+							super.execute();
+						}
+					});
+
+					this.add(new com.ibm.rational.test.lt.execution.protocol.impl.HTTPUserComplete(this));
+					super.execute();
+				}
+			};
+
+			return scenario;
+		}
+
+	}
+
+}
